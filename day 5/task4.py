391799a6492a410f453c4efc427150c30a70117d
ruhinvahora68-lang <ruhinvahora68@gmail.com>
2026-08-31 16:10:33 +0530

    commit;

--- day 5/task4.py
+++ day 5/task4.py
@@ -1,3 +1,4 @@
+
 class Employee:
     def __init__(self, salary):
         self.__salary = salary
---
+++ text yo handwriting/demo.py
@@ -0,0 +1,6 @@
+import pywhatkit as pw
+# txt="yamin and saifil"
+pw.text_to_handwriting("My name is saifil","demo25.png",(0, 0, 138))
+# print) 
+
+
